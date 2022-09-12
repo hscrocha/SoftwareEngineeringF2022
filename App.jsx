827f05bf0281325f3a1ef28141fce60f72da1530
Henrique Rocha <henrique.rocha@gmail.com>
2022-09-11 23:07:22 -0400

    Add Screens support Draft of Main Menu

--- App.jsx
+++ App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import HelloWorld from "./view/HelloWorld";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainMenuScreen from "./view/screens/MainMenuScreen";
+import HelloWorld from "./view/components/HelloWorld";
 
-export default function App(){
+export default function App() {
+    const Stack = createNativeStackNavigator();
+  
     return (
-        <HelloWorld>Hello React Native!</HelloWorld>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Main Menu" component={MainMenuScreen} />
+          <Stack.Screen
+          name="Hello World"
+          component={HelloWorld}
+          options={{title: 'Hello'}}
+          initialParams={{'text':'Hello React!'}}
+          > 
+          </Stack.Screen>
+        </Stack.Navigator>      
+      </NavigationContainer>
     );
 }
---
+++ view/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HelloWorld(props){
+    let message = "Hello World";
+    if(props.children) message = props.children;
+    else if(props.text) message = props.text;
+    else if(props.route.params.text) message = props.route.params.text;
+
+    return (
+        <View style={{flex: 1,justifyContent:'center',alignItems:'center'}}>
+            <Text style={{fontSize:20}}> {message} </Text>
+        </View>
+    );
+}
